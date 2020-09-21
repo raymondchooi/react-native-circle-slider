50e393347b1fae5ab95e0679fade8e02271d16bc
Raymond Chooi <raymond_chooi282@hotmail.com>
2020-09-21 01:13:12 +0100

    Update index to just export CircleSlider

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-'use strict';
+import CircleSlider from "./CircleSlider";
 
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import CircleSlider from './CircleSlider';
-
-export default class CircleSliderContainer extends Component {
-
-  render() {
-    return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <CircleSlider
-          value={90}
-        />
-      </View>
-    )
-  }
-}
+export default CircleSlider;
