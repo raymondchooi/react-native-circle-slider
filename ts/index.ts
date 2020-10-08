e2caba32a07f02007ac7a214ac15b8881e617d3c
Raymond Chooi <raymond_chooi282@hotmail.com>
2020-10-08 08:18:23 +0100

    Delete test ts convert

--- ts/index.ts
+++
@@ -1,4 +0,0 @@
-// @ts-expect-error ts-migrate(6142) FIXME: Module './CircleSlider' was resolved to '/Users/ra... Remove this comment to see the full error message
-import CircleSlider from "./CircleSlider";
-
-export default CircleSlider;
